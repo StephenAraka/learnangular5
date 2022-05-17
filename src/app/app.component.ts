e954de0ecbab16c177601b484a65221d97690ca1
StephenAraka <stevenaraka@gmail.com>
2022-05-17 12:20:15 +0200

    use template url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,7 @@ import { Component } from '@angular/core';
 // Angular templates can either use in file html or URL referencing a html file
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: `
-  <div class="row justify-content-center">
-  <div class="card mt-sm-3 col-md-8">
-    <div class="card-body">
-      <h3 class="mb-0">Artist Directory</h3>
-      <div class="form-group">
-        <div class="form-label"></div>
-        <input class="form-control mt-2" type="text">
-      </div><!-- form-group -->
-    </div><!-- card-body -->
-  </div><!-- card -->
-</div><!-- row -->
-`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
